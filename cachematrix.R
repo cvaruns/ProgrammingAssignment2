4fb28b008324830e7be5448780452b526da54b06
cvaruns <cvaruns@users.noreply.github.com>
2015-06-19 14:43:34 -0400

    Update cachematrix.R
    Edited code and added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to create a cache matrix, store it and return the
+## inverse of the cached matrix stored.
 
-## Write a short comment describing this function
+## makeCacheMatrix is the function used to take a 'square matrix' as an argument and create a cache matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix())  ##Accepts only Square Matrix
+  { 
+  i <- NULL
+  y <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() ##Stores the matrix in cache
+  {
+    x
+  }
+  setinverse <- function(inv){ i <<- inv}
+  getinverse <- function(){ i}
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is the function that generates and returns the inverse of the cache matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) ## Return a matrix that is the inverse of 'x'
+{
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data.")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data,...) ##Obtaining Inverse of Cached Matrix
+  x$setinverse(i)
+  i
 }
+##This function only works if the Determinant of the cached matrix is something other than 0.
+##Else it will generate an error.
